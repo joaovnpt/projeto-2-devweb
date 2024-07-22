d40349ca6e1a021ad2ceeb0f78dc289a3a532dff
joaovnpt <joaovitorguiotti8@gmail.com>
2024-07-22 15:23:30 -0300

    feito as duas divs no RegisterForm

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { computed, ref } from 'vue';
+import RegisterForm from './components/RegisterForm.vue'
+
+
+</script>
+
+<template>
+
+  <div class="container">
+    <register-form/>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+    const titulo = "Projeto 2 - Componentes"
+
+</script>
+
+<template>
+    <div class="form-container">
+        <h1 class="titulo">Registre-se em</h1>
+        <h1 class="titulo">{{ titulo }}</h1>
+    </div>
+    <div class="results-container">
+
+    </div>
+</template>
+
+<style scoped>
+
+.form-container {
+    width: 50%;
+}
+
+.results-container {
+    width: 50%;
+    display: flex;
+    justify-self: flex-start;
+    color: black;
+}
+
+</style>
