cab0f73b6c97c10835b14c45636f80ac38fa8057
joaovnpt <joaovitorguiotti8@gmail.com>
2024-07-25 01:39:32 -0300

    implementado funções de verificação de: senha, hobbies e campos vazios

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -44,25 +44,58 @@ const states = [
   { uf: 'SE', name: 'Sergipe' },
   { uf: 'TO', name: 'Tocantins' }
 ]
+
+function passwordVerification() {
+  if (user.password != user.confirmPassword) {
+    alert('Passwords must be the same.');
+
+  }
+}
+
+function hobbiesVerification() {
+  const caracter = ",";
+  if (!user.hobbies.includes(caracter)) alert('You must have at least 2 hobbies.')
+}
+
+function emptyFieldVerification() {
+  for (const key in user) {
+    if (!user[key] || user[key].trim()) { //confere se alguma key (dado dentro do objeto) não está vazia.
+      alert('aaa');
+      return; //para a execução se encontrar um campo vazio, assim não apresentará 'aaa' 100 vezes.
+    }
+  }
+}
+
+function generalVerification() {
+  passwordVerification();
+  hobbiesVerification();
+  emptyFieldVerification()
+}
 </script>
 
 <template>
   <div class="form-container">
     <h1 class="title">Register on</h1>
     <h1 class="title">{{ title }}</h1>
-    <form action="" class="form">
-      .
+    <form action="" class="form" @submit.prevent="generalVerification()">
+      <!--@submit.prevent="..." impede que o formulário seja enviado, caso não passe da verificação -->
+
       <label for="name">Name:</label>
       <input type="text" name="name" id="name" v-model="user.name" />
 
       <label for="email">Email:</label>
-      <input type="text" name="name" id="email" v-model="user.email" />
+      <input type="email" name="name" id="email" v-model="user.email" />
 
       <label for="password">Password:</label>
       <input type="password" name="password" id="password" v-model="user.password" />
 
       <label for="confirmPassword">Confirm your password:</label>
-      <input type="password" name="confirmPassword" id="confirmPassword" v-model="user.confirmPassword" />
+      <input
+        type="password"
+        name="confirmPassword"
+        id="confirmPassword"
+        v-model="user.confirmPassword"
+      />
 
       <label for="adress">Adress:</label>
       <input type="adress" name="adress" id="adress" v-model="user.adress" />
@@ -79,17 +112,29 @@ const states = [
       </select>
 
       <label for="birthDate">Birth date:</label>
-      <input type="date" name="birthDate" id="birthDate" v-model="user.birthDate">
+      <input type="date" name="birthDate" id="birthDate" v-model="user.birthDate" />
 
       <label for="hobbies">Hobbies:</label>
-      <input type="text" name="hobbies" id="hobbies" v-model="user.hobbies">
+      <input type="text" name="hobbies" id="hobbies" v-model="user.hobbies" />
 
       <label for="programmingLanguages">Programming languages:</label>
-      <input type="text" name="programmingLanguages" id="programmingLanguages" v-model="user.programmingLanguages">
+      <input
+        type="text"
+        name="programmingLanguages"
+        id="programmingLanguages"
+        v-model="user.programmingLanguages"
+      />
 
       <label for="biography">Biography:</label>
-      <textarea name="biography" id="biography" cols="25" rows="10" v-model="user.biography"></textarea>
-      
+      <textarea
+        name="biography"
+        id="biography"
+        cols="25"
+        rows="10"
+        v-model="user.biography"
+      ></textarea>
+
+      <button class="show-data-btn">Show data</button>
     </form>
   </div>
 </template>
@@ -106,6 +151,6 @@ const states = [
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;;
+  flex-direction: column;
 }
 </style>
